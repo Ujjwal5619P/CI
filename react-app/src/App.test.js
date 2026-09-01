9639b00524e0719206e5d722a345c110788c008f
Ujjwal Chauhan <ujjwal892360@gmail.com>
2026-09-01 11:40:55 +0530

    Update App.test.js

--- react-app/src/App.test.js
+++ react-app/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/aabbcc/i);
-  expect(linkElement).toBeInTheDocument();
+const { getByText } = render();
+const linkElement = getByText((content, element) => content.startsWith('aabbcc'));
 });
